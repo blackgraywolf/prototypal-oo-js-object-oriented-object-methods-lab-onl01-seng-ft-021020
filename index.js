d7ab522667ca947937afb907a2a22217aa75078f
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-05-29 17:02:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function BoardMember(name, homeState, training){
     this.training = training;
 }
 
-BoardMember.prototype.veto = function(){
+BoardMember.veto = function(){
     return "No, I must disagree"
 }
 
